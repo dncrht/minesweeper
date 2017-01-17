4ad8d5265909b679e4268c7d4d365eeb73d6a358
Dani <daniel.cr.ht@gmail.com>
2017-01-17 08:45:43 +0000

    Extract function that reveals a position from game loop and every adjacent square

--- ms.rb
+++ ms.rb
@@ -8,24 +8,13 @@ NUM = 3
 mines = Array.new(ROWS) { Array.new(COLS) }
 @playfield = Array.new(ROWS) { Array.new(COLS) }
 
-# no vale pq se repiten
-#NUM.times do
-#  mines[rand(ROWS)][rand(COLS)] = true
-#end
-
 c = NUM
-begin
+until c.zero?
   x = rand(COLS)
   y = rand(ROWS)
   unless mines[y][x]
     mines[y][x] = true
     c -= 1
-  end
-end until c.zero?
-
-def p_m(mines)
-  mines.each do |row|
-    puts row.map { |c| c ? 'X' : '.' }.join
   end
 end
 
@@ -54,14 +43,6 @@ def minecounter(mines, y, x)
   count
 end
 
-def tally(mines)
-  mines.each_with_index do |row, y|
-    x = -1
-    puts row.map { |_| x += 1;
-    minecounter(mines, y, x)}.join
-  end
-end
-
 # game loop
 def reveal_adjacent(mines, y, x)
   (-1).upto(1).each do |dx|
@@ -72,15 +53,7 @@ def reveal_adjacent(mines, y, x)
       next if x + dx >= COLS
       next if y + dy >= ROWS
 
-      content = minecounter(mines, y + dy, x + dx)
-      @playfield[y + dy][x + dx] = content
-      #p_p
-      #puts @visited.inspect
-      #gets
-      if @playfield[y + dy][x + dx].zero? && !@visited.include?([y + dy, x + dx])
-        @visited << [y + dy, x + dx]
-        reveal_adjacent(mines, y + dy, x + dx)
-      end
+      reveal_position(mines, y + dy, x + dx)
     end
   end
 end
@@ -89,12 +62,17 @@ def left
   @playfield.flatten.select(&:nil?).size
 end
 
+def reveal_position(mines, y, x)
+  content = minecounter(mines, y, x)
+  @playfield[y][x] = content
+  if content.zero? && !@visited.include?([y, x])
+    @visited << [y, x]
+    reveal_adjacent(mines, y, x)
+  end
+end
+
 loop do
   p_p
-  puts
-  p_m mines
-  puts
-  tally mines
   puts
   input = gets.strip
   if input.include? ','
@@ -104,12 +82,7 @@ loop do
       puts 'lose!'
       exit 1
     end
-    content = minecounter(mines, y, x)
-    @playfield[y][x] = content
-    @visited << [y, x]
-    if content.zero? # reveal adjacent
-      reveal_adjacent(mines, y, x)
-    end
+    reveal_position(mines, y, x)
 
     if left == NUM
       puts 'the winner is you!'
