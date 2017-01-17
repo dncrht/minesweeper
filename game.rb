b134258f589cf1951ecb80515e0289600ffeffb9
Dani <daniel.cr.ht@gmail.com>
2017-01-17 14:38:44 +0000

    More command line niceties

---
+++ game.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require_relative 'playfield'
+
+class Game
+  ROWS = 5
+  COLS = 10
+  NUM = 3
+
+  def initialize(argv)
+    @playfield = Playfield.new(
+      mine_number: argv[0] || NUM,
+      rows:        argv[1] || ROWS,
+      cols:        argv[2] || COLS,
+    )
+  end
+
+  def play
+    @playfield.display
+
+    loop do
+      puts "Enter position to reveal as: y, x"
+      input = STDIN.gets.strip
+
+      if input.include? ','
+        y, x = input.split(',').map &:to_i
+
+        if @playfield.has_mine?(y, x)
+          puts "\n\nLose!"
+          exit 1
+        end
+        @playfield.reveal_square(y, x)
+        puts
+        @playfield.display
+
+        if @playfield.squares_left?
+          puts "\n\nThe winner is you!"
+          exit 0
+        end
+      end
+    end
+  end
+end
+
+if $0.include? 'game.rb'
+  Game.new(ARGV.map &:to_i).play
+end
